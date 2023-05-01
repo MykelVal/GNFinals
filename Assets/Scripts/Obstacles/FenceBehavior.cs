dc0140c7ffc28f7c41e373e914217efd80f7ce1a
MykelVal <mykel.valencia@ciit.edu.ph>
2023-05-01 23:39:04 +0800

    optimized obstacle scripts

--- Assets/Scripts/Obstacles/FenceBehavior.cs
+++ Assets/Scripts/Obstacles/FenceBehavior.cs
@@ -7,19 +7,37 @@ public class FenceBehavior : MonoBehaviour
     [Header("Activator Button")]
     [SerializeField] private ButtonBehavior button;
 
+    [SerializeField] private bool isReversed = false;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (button.isPressed())
+        if (isReversed)
         {
-            this.GetComponent<SpriteRenderer>().enabled = false;
-            this.GetComponent<PolygonCollider2D>().enabled = false;
+            if (button.isPressed())
+            {
+                this.GetComponent<SpriteRenderer>().enabled = true;
+                this.GetComponent<PolygonCollider2D>().enabled = true;
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().enabled = false;
+                this.GetComponent<PolygonCollider2D>().enabled = false;
+            }
         }
         else
         {
-            this.GetComponent<SpriteRenderer>().enabled = true;
-            this.GetComponent<PolygonCollider2D>().enabled = true;
+            if (button.isPressed())
+            {
+                this.GetComponent<SpriteRenderer>().enabled = false;
+                this.GetComponent<PolygonCollider2D>().enabled = false;
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().enabled = true;
+                this.GetComponent<PolygonCollider2D>().enabled = true;
+            }
         }
     }
 }
